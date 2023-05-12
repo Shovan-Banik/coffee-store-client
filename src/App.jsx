888adbd40419de1f9cbbd4d037b19bc09c6cb976
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-05-12 11:48:52 +0600

    show data from database and delete from DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import CoffeeCard from './components/CoffeeCard';
 
 function App() {
+  const coffees= useLoaderData();
   return (
-    <>
-      <h1 className='text-6xl text-purple-600'>Coffee Store</h1>
-    </>
+    <div className='m-20'>
+      <h1 className='text-6xl text-purple-600 text-center mb-12 font-bold'>Coffee Store</h1>
+     <div className='grid md:grid-cols-2 gap-5'>
+     {
+        coffees.map(coffee=><CoffeeCard
+         key={coffee._id}
+         coffee={coffee}>
+        </CoffeeCard>)
+      }
+     </div>
+    </div>
   )
 }
 
